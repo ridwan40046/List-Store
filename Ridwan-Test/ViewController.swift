654bb844c7ece9849730b3da0eea24da7530de16
Ridwan <ridwanputra72@gmail.com>
2022-12-26 18:30:04 +0700

    Initial Commit

---
+++ Ridwan-Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ridwan-Test
+//
+//  Created by Ridwan Surya Putra on 26/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
